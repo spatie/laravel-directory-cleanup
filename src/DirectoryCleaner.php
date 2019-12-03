c33234e473e92ae219e333d370a5a1ed13870dc9
Thorsten Daners <thorsten.daners@antwerpes.de>
2019-12-03 17:41:08 +0100

    Styleguide.

--- src/DirectoryCleaner.php
+++ src/DirectoryCleaner.php
@@ -68,7 +68,7 @@ class DirectoryCleaner
     {
         return collect($this->filesystem->directories($this->directory))
             ->filter(function ($directory) {
-                return !$this->filesystem->allFiles($directory, true);
+                return ! $this->filesystem->allFiles($directory, true);
             })
             ->each(function ($directory) {
                 $this->filesystem->deleteDirectory($directory);
